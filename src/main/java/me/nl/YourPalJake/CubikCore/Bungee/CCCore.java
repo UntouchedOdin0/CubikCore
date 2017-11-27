0a28432333a3faf13bac5b3c9180f2b467cfa942
Jake Nijssen <jtmnijssen@outlook.com>
2017-11-27 17:47:30 +0100

    Initial commit

---
+++ src/main/java/me/nl/YourPalJake/CubikCore/Bungee/CCCore.java
@@ -0,0 +1,16 @@
+package me.nl.YourPalJake.CubikCore.Bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class CCCore extends Plugin{
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
---
+++ src/main/java/me/nl/YourPalJake/CubikCore/Spigot/CCCore.java
@@ -0,0 +1,18 @@
+package me.nl.YourPalJake.CubikCore.Spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CCCore extends JavaPlugin{
+
+
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
