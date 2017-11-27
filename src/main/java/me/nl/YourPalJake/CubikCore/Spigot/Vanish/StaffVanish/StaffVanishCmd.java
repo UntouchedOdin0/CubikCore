7e1ec0cd19e586b373477c14169b11926bc8af05
Jake Nijssen <jtmnijssen@outlook.com>
2017-11-27 18:02:55 +0100

    Began on Vanish + StaffVanish

---
+++ src/main/java/me/nl/YourPalJake/CubikCore/Spigot/Vanish/StaffVanish/StaffVanishCmd.java
@@ -0,0 +1,13 @@
+package me.nl.YourPalJake.CubikCore.Spigot.Vanish.StaffVanish;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class StaffVanishCmd implements CommandExecutor{
+
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        return true;
+    }
+}
---
+++ src/main/java/me/nl/YourPalJake/CubikCore/Spigot/Vanish/Vanish/VanishCmd.java
@@ -0,0 +1,13 @@
+package me.nl.YourPalJake.CubikCore.Spigot.Vanish.Vanish;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class VanishCmd implements CommandExecutor{
+
+
+    public boolean onCommand(CommandSender sender, Command cmd, String lablel, String[] args){
+        return true;
+    }
+}
